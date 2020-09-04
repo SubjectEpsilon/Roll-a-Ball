69042aa0ce7806ff9aa12d17c5ea0ac9313922fa
Corbin Bostian <corbinbostian@mailcom>
2020-09-04 16:55:35 -0500

    StraightEmpty now spawns a new cell when entered.

--- Assets/Scripts/RaiseBlockade.cs
+++ Assets/Scripts/RaiseBlockade.cs
@@ -6,6 +6,16 @@ public class RaiseBlockade : MonoBehaviour
 {
     public GameObject blockade;
     public GameObject blockadeTrigger;
+    public GameObject thisCell;
+    public GameObject nextCell;
+
+
+    private void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("PlayerTag")){
+            GameObject cell = Instantiate(nextCell);
+            cell.transform.position = thisCell.transform.position + new Vector3(0,0,51);
+        }
+    }
     private void OnTriggerExit(Collider other){
         if (other.gameObject.CompareTag("PlayerTag")){
             blockadeTrigger.SetActive(false);
